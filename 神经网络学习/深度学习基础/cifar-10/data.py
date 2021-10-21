224155327fae1c70522c172fc4c28bf286aa5c18
Apricity <56157591+Spphire@users.noreply.github.com>
2021-10-21 22:29:03 +0800

    Add files via upload

---
+++ 神经网络学习/深度学习基础/cifar-10/data.py
@@ -0,0 +1,65 @@
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import struct
+import os
+
+
+
+class MNIST(Dataset):
+    def __init__(self, path, mode):
+
+        if True :
+            with open(os.path.join(path, f"{mode}_batch"), "rb") as fp:
+                self.xs = np.fromfile(fp, dtype=np.uint8).reshape(-1, 3 * 32 * 32 + 1).astype(np.float32)[ : , 1::1].reshape(-1 , 3 , 32 , 32)
+            with open(os.path.join(path, f"{mode}_batch"), "rb") as fp:
+                self.ys = np.fromfile(fp, dtype=np.uint8).reshape(-1, 3 * 32 * 32 + 1).astype(np.float32)[ : , 0].reshape(-1)
+            return
+        if mode=="train" :
+
+            with open(os.path.join(path, f"{mode}_batch_1"), "rb") as fp:
+                xs1 = np.fromfile(fp, dtype=np.uint8).reshape(-1, 3 * 32 * 32 + 1).astype(np.float32)[:,1::1].reshape(-1, 3, 32, 32)
+            with open(os.path.join(path, f"{mode}_batch_1"), "rb") as fp:
+                ys1 = np.fromfile(fp, dtype=np.uint8).reshape(-1, 3 * 32 * 32 + 1).astype(np.float32)[:,0].reshape(-1)
+
+            with open(os.path.join(path, f"{mode}_batch_2"), "rb") as fp:
+                xs2 = np.fromfile(fp, dtype=np.uint8).reshape(-1, 3 * 32 * 32 + 1).astype(np.float32)[:,1::1].reshape(-1, 3, 32, 32)
+            with open(os.path.join(path, f"{mode}_batch_2"), "rb") as fp:
+                ys2 = np.fromfile(fp, dtype=np.uint8).reshape(-1, 3 * 32 * 32 + 1).astype(np.float32)[:,0].reshape(-1)
+
+            with open(os.path.join(path, f"{mode}_batch_3"), "rb") as fp:
+                xs3 = np.fromfile(fp, dtype=np.uint8).reshape(-1, 3 * 32 * 32 + 1).astype(np.float32)[:,1::1].reshape(-1, 3, 32, 32)
+            with open(os.path.join(path, f"{mode}_batch_3"), "rb") as fp:
+                ys3 = np.fromfile(fp, dtype=np.uint8).reshape(-1, 3 * 32 * 32 + 1).astype(np.float32)[:,0].reshape(-1)
+
+            with open(os.path.join(path, f"{mode}_batch_4"), "rb") as fp:
+                xs4 = np.fromfile(fp, dtype=np.uint8).reshape(-1, 3 * 32 * 32 + 1).astype(np.float32)[:,1::1].reshape(-1, 3, 32, 32)
+            with open(os.path.join(path, f"{mode}_batch_4"), "rb") as fp:
+                ys4 = np.fromfile(fp, dtype=np.uint8).reshape(-1, 3 * 32 * 32 + 1).astype(np.float32)[:,0].reshape(-1)
+
+            with open(os.path.join(path, f"{mode}_batch_5"), "rb") as fp:
+                xs5 = np.fromfile(fp, dtype=np.uint8).reshape(-1, 3 * 32 * 32 + 1).astype(np.float32)[:,1::1].reshape(-1, 3, 32, 32)
+            with open(os.path.join(path, f"{mode}_batch_5"), "rb") as fp:
+                ys5 = np.fromfile(fp, dtype=np.uint8).reshape(-1, 3 * 32 * 32 + 1).astype(np.float32)[:,0].reshape(-1)
+
+            self.xs = np.concatenate((xs1,xs2,xs3,xs4,xs5),axis=0)
+            self.ys = np.concatenate((ys1, ys2, ys3, ys4, ys5), axis=0)
+    def __len__(self):
+        return self.xs.shape[0]
+
+    def __getitem__(self, item):
+        return self.xs[item] / 255., self.ys[item]
+        # [1x28x28] numpy            [] 
+        # batch=128
+        # stack([]) -> [128x1x28x28] tensor
+        # stack()   -> [128] tensor
+
+def create_train_loader(batch_size):
+    data = MNIST(path="MNIST", mode="train")
+    loader = DataLoader(data, batch_size, shuffle=True)
+    return loader
+
+
+def create_test_loader(batch_size):
+    data = MNIST(path="MNIST", mode="test")
+    loader = DataLoader(data, batch_size)
+    return loader
---
+++ 神经网络学习/深度学习基础/cifar-10/train.py
@@ -0,0 +1,66 @@
+from model import FcModel, CnnModel, EmaModel, init_model, ModelImage
+from data import create_test_loader, create_train_loader
+from assess import test
+
+import torch.optim as optim
+import torch.nn as nn
+import torch
+
+import matplotlib.pyplot as plt
+import seaborn 
+
+import tqdm
+import os
+
+torch.backends.cudnn.benchmark = True
+
+def train(model, batch, epoch, lr, device="cpu"):
+    os.makedirs("runs", exist_ok=True)
+
+    train_loader = create_train_loader(batch_size=batch)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    loss_func = nn.CrossEntropyLoss()
+
+    model.to(device)
+    ema_model = EmaModel(model, 0.999)
+
+    best_criteria = 0
+
+    bar = tqdm.trange(epoch, desc="epoch")
+    for e in bar:
+        model.train()
+        for xs, ys in tqdm.tqdm(train_loader, desc="train"):
+            # xs: [Bx1x28x28]
+            # ys: [B]
+            optimizer.zero_grad()
+            inputs = xs.to(device)
+            labels = ys.to(device)  # [B]
+            labels = labels.long()
+            outputs = model(inputs) # [Bx10]
+            loss = loss_func(outputs, labels)
+            loss.backward()     # dl/dtheta
+            optimizer.step()    # update parameter
+            ema_model.update(model)
+        
+        result = test(ema_model, batch, device)
+        precision = result["mean-precision"]
+        recall = result["mean-recall"]
+        confuse = result["total-confuse"].cpu().numpy()
+        bar.set_postfix({"precision": precision, 
+                         "recall": recall})
+
+        criteria = (precision + recall) / 2
+        if criteria > best_criteria:
+            best_criteria = criteria
+            torch.save(ema_model, f"runs/{model.__class__.__name__}-best.pt")
+        torch.save(ema_model, f"runs/{model.__class__.__name__}-last.pt")
+    
+    seaborn.heatmap(confuse, annot=True, fmt="d")
+    plt.savefig(f"runs/{model.__class__.__name__}-confuse.jpg")
+
+
+if __name__ == "__main__":
+    model = ModelImage()
+    # model = CnnModel()
+    init_model(model)
+    train(model, 128, 7, 0.01, "cpu")
